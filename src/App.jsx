c0ed303b2f6bdd5439b9eb978ff4625ac782a0e4
Kinjal_Shah <kinjalshah83415@gmail.com>
2023-10-27 11:53:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import {InputBox} from './components'
+import './App.css'
+import  useCurrencyInfo from './hooks/useCurrencyInfo'
+
+function App() {
+ 
+  const [amount,setAmount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
